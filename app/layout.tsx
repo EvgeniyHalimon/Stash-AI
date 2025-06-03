1dadfb41a34b56943082fd6b2e0f9946c4a9ca85
EHalimon <evgeniyhalimon321@gmail.com>
2025-06-03 15:08:39 +0300

    added tanstack

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Navigation } from '@/components';
+import Providers from '@/shared/provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,10 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex flex-col xl:h-dvh xl:flex-row">
-          <Navigation />
-          {children}
-        </div>
+        <Providers>
+          <div className="flex flex-col xl:h-dvh xl:flex-row">
+            <Navigation />
+            {children}
+          </div>
+        </Providers>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
+'use client';
 import { Calendar, StashTable, Chart } from '@/components';
+import { useQuery } from '@tanstack/react-query';
+
+async function fetchPosts() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!res.ok) {
+    throw new Error('Failed to fetch posts');
+  }
+  return res.json();
+}
 
 export default function Home() {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  });
+  console.log(
+    '🚀 ~ Home ~ data, isLoading, isError, error :',
+    data,
+    isLoading,
+    isError,
+    error,
+  );
   return (
     <div className="grid w-full gap-5 p-4 xl:grid-cols-2 xl:grid-rows-2">
       <Calendar />
